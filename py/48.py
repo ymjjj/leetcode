23f9f378f8bbbbd189dd4e0fbd6366a28d6107f4
GitHub ymjjj <GitHub 2170768863@qq.com>
2024-08-07 12:13:32 +0800

    48

---
+++ py/48.py
@@ -0,0 +1,15 @@
+from typing import List
+from typing import List
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+
+        for i in range(n // 2):
+            for j in range((n + 1) // 2):
+                matrix[i][j], matrix[n - j - 1][i], matrix[n - i - 1][n - j - 1], matrix[j][n - i - 1] = matrix[n - j - 1][i], matrix[n - i - 1][n - j - 1], matrix[j][n - i - 1], matrix[i][j]
+
+matrix=[[1,2,3],[4,5,6],[7,8,9]]
+solution=Solution()
+solution.rotate(matrix)
+print(matrix)
